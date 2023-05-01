76907d790146f73e917f17c1d8e5323f20715098
matteorave <matteorave96@gmail.com>
2023-05-01 17:50:43 +0200

    commit es2

---
+++ es2.cs
@@ -0,0 +1,20 @@
+            int n = 0;  
+            string s = "";
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+            string risultato = "";
+            foreach (char c in s)
+            {
+                int posizione = alfabeto.IndexOf(c);
+                if (posizione >= 0)
+                {
+                    posizione = (posizione - n) % alfabeto.Length;
+
+                    risultato += alfabeto[posizione];
+                }
+                else
+                {
+
+                    risultato += c;
+
+                }
+            }
