367c5148a5a314cf691320a66458dbf47f78a4c7
matteorave <matteorave96@gmail.com>
2023-05-01 17:52:59 +0200

    commit es6

---
+++ es6.cs
@@ -0,0 +1,20 @@
+static void es6
+          
+          { 
+            int n = 0;
+            int C = 0;
+            string s = "";
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+            double risultato = 0;
+            if (n % 2==0) 
+            {
+               es3();
+            }
+            else 
+            { 
+               es4();          
+            }
+            C++;
+            Console.Write(C);
+            
+          }
