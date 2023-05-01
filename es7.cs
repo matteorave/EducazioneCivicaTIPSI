381c1d7f4e4c64f594e8670227a87717f164f52e
matteorave <matteorave96@gmail.com>
2023-05-01 17:53:36 +0200

    commit es7

---
+++ es7.cs
@@ -0,0 +1,18 @@
+            int n = 0;
+            string s = "";
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+            int risultato = 0;
+             foreach (char c in s)
+            {
+                int posizione = alfabeto.IndexOf(c);
+                if (posizione >= 0)
+                {
+                    posizione = posizione % alfabeto.Length;
+                    es5();
+                    Console.Write(s);              
+                }
+                else
+                {
+                    risultato += c;
+                }
+            }
