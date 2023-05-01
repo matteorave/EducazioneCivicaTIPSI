668e7e5754549dfa3b61e20b2d08f8cbab3a062f
matteorave <matteorave96@gmail.com>
2023-05-01 17:51:44 +0200

    commit es4

---
+++ es4.cs
@@ -0,0 +1,25 @@
+static void es4
+        { 
+            int n = 0;
+            string s = "";
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+            double risultato = 0;
+            foreach (char c in s)
+            {
+                int posizione = alfabeto.IndexOf(c);
+                if (posizione >= 0)
+                {
+                    posizione = posizione % alfabeto.Length;
+
+                    risultato =risultato * posizione;
+                }
+                else
+                {
+
+                    risultato += c;
+
+                }
+            }
+            risultato = risultato / n;
+                               
+    }
